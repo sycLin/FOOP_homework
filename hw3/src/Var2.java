39c24328ebd521a2b79bc2e9eda157e87cb6e9fa
sycLin <b01902044@ntu.edu.tw>
2015-11-16 15:55:44 +0800

    + Variance 2 implemented

---
+++ hw3/src/Var2.java
@@ -0,0 +1,36 @@
+import java.lang.*;
+import java.util.*;
+
+/**
+ * Variance 2: different player number.
+ */
+public class Var2 extends OldMaidGame {
+	/**
+	 * Constructor
+	 */
+	public Var2() {
+		this.playerCount = 6;
+		initDeck();
+		initPlayers();
+	}
+
+	/**
+	 * Constructor 2: given number of players
+	 * @param playerNum how many players
+	 */
+	public Var2(int playerNum) {
+		this.playerCount = playerNum;
+		initDeck();
+		initPlayers();
+	}
+
+	/**
+	 * overriding because we need two decks
+	 */
+	@Override
+	public void initDeck() {
+		this.deck = new Deck();
+		Deck anotherDeck = new Deck();
+		this.deck.addAll(anotherDeck.cards);
+	}
+}
